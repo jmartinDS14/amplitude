9bace5cdf51753188d9a25cd458097d8c74f24bb
jmartinDS14 <jenny.martin@theinformationlab.co.uk>
2025-01-30 12:06:01 +0000

    Dynamic dates

--- amplitude_s3_list.py
+++ amplitude_s3_list.py
@@ -1,10 +1,6 @@
-#Data Extraction using Amplitude Export API
-#https://amplitude.com/docs/apis/analytics/export
-
-import os
-import requests
-from dotenv import load_dotenv
 import boto3
+import os
+from dotenv import load_dotenv # For reading .env file
 import re
 from datetime import datetime, timedelta
 
@@ -35,23 +31,3 @@ start_time = next_day.strftime('%Y%m%dT%H')
 today = datetime.today()
 today = today.replace(hour=0, minute=0, second=0, microsecond=0)
 end_time = today.strftime('%Y%m%dT%H')
-
-api_key=os.getenv('AMP_API_KEY')
-api_secret_key=os.getenv('AMP_SECRET_KEY')
-
-
-url='https://analytics.eu.amplitude.com/api/2/export'
-params = {
-    'start': start_time,
-    'end': end_time
-}
-
-response = requests.get(url, params=params, auth=(api_key,api_secret_key))
-
-if response.status_code == 200:
-    data = response.content
-    print('Data retrieved successfully.')
-    with open('data.zip', 'wb') as file:
-        file.write(data)
-else:
-    print(f'Error {response.status_code}: {response.text}')
